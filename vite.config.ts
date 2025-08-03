8565f8e9eeb717d67b98baf1c4ebcd5ab0f177dc
isMaya404 <franciajeremiah10@gmail.com>
2025-08-03 23:33:38 +0800

    chore(config): update config files

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
     },
-  },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
