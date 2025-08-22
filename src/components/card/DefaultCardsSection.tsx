3ec96b440c6850fb42c36c61aa72999f19701c0b
isMaya404 <franciajeremiah10@gmail.com>
2025-08-22 18:26:52 +0800

    use abstracted CardPopup on DefaultCardSection

--- src/components/card/DefaultCardsSection.tsx
+++ src/components/card/DefaultCardsSection.tsx
@@ -9,6 +9,7 @@ import type {
     HomePageQuery,
     HomePageQueryVariables,
 } from "@/lib/anilist/gql/graphql";
+import CardPopup from "./CardPopup";
 
 type DefaultCardsSectionProps = {
     qk: string;
@@ -75,53 +76,8 @@ const DefaultCardsSection = ({
 
                     return (
                         <div key={m.id} className="relative w-full">
-                            {/* TODO: abstract card popup */}
-
-                            {/* Card popup */}
                             {isLgAndUp && hoveredId === m.id && (
-                                <div
-                                    className={cn(
-                                        popupSide === "left"
-                                            ? "right-[110%]"
-                                            : "left-[110%]",
-                                        "flex flex-col justify-evenly align-center card-popup-anim absolute top-[1rem] z-10 bg-white dark:bg-sec p-4 rounded-lg shadow-md h-[170px] w-[320px]"
-                                    )}
-                                >
-                                    {/* Card popup contents */}
-                                    <div className="flex justify-between text-16-medium">
-                                        <div>
-                                            Ep airing in{" "}
-                                            {/* timeUntilAiring is in seconds so it's divided by 86400, the num of seconds in a day, */}
-                                            {/* then flooring it getting only a whole number number of days */}
-                                            {Math.floor(
-                                                (m?.nextAiringEpisode
-                                                    ?.timeUntilAiring ?? 0) /
-                                                    86400
-                                            )}
-                                        </div>{" "}
-                                        <div>86%</div>
-                                    </div>
-
-                                    <div className="flex flex-col gap-2 text-14-normal">
-                                        <div>CloverWorks</div>{" "}
-                                        <div>TV SHOW - 12 episodes</div>
-                                    </div>
-
-                                    <div className="flex gap-2 text-14-normal">
-                                        <div>foo</div> <div>bar</div>
-                                        <div>foo</div>
-                                    </div>
-
-                                    {/* Card Popup tail */}
-                                    <div
-                                        className={cn(
-                                            popupSide === "left"
-                                                ? "-right-2 border-l-10 border-l-white dark:border-l-sec"
-                                                : "-left-2 border-r-10 border-r-white dark:border-r-sec",
-                                            "absolute top-6 w-0 h-0 border-y-10 border-y-transparent"
-                                        )}
-                                    ></div>
-                                </div>
+                                <CardPopup media={m} popupSide={popupSide} />
                             )}
 
                             <Card
