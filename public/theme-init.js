ccc39f9d01291c66205d2949ec8ec297e78e12fa
isMaya404 <franciajeremiah10@gmail.com>
2025-09-01 14:02:33 +0800

    added some comments on for future manual

--- public/theme-init.js
+++ public/theme-init.js
@@ -1,12 +1,20 @@
-(function () {
-  const stored = localStorage.getItem("theme");
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const theme =
-    stored && stored !== "system" ? stored : prefersDark ? "dark" : "light";
-  document.documentElement.setAttribute("data-theme", theme);
+// This file should be called inside the head index.html
+// like so: <script src="/theme-init.js"></script>
 
-  const style = document.createElement("style");
-  style.textContent = `
+(function () {
+    const stored = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)",
+    ).matches;
+
+    // Use the stored value if there's any (this is the value the user
+    // explicitly set). Otherwise respect the OS and use that
+    const theme =
+        stored && stored !== "system" ? stored : prefersDark ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+
+    const style = document.createElement("style");
+    style.textContent = `
     :root {
       --fg: oklch(0.145 0 0); /* dark */
       --prim: oklch(0.985 0 0); /* light */
@@ -20,5 +28,6 @@
       color: var(--fg);
     }
   `;
-  document.head.appendChild(style);
+    // append to head to avoid FOUC
+    document.head.appendChild(style);
 })();
