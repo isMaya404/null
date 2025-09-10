4d91c164c105694ef486b5b73e36c1d3e364d59f
isMaya404 <franciajeremiah10@gmail.com>
2025-09-10 17:54:23 +0800

    fix getNextSeason index

--- src/lib/utils/dates.ts
+++ src/lib/utils/dates.ts
@@ -4,17 +4,14 @@ export const getSeasonFromMonthNumber = (monthNumber: number): MediaSeason => {
     if (monthNumber < 4) return MediaSeason.Winter;
     if (monthNumber < 7) return MediaSeason.Spring;
     if (monthNumber < 10) return MediaSeason.Summer;
-
     return MediaSeason.Fall;
 };
 
 export const getCurrentSeason = (): MediaSeason => {
     const month = new Date().getMonth() + 1;
-
     if (month < 4) return MediaSeason.Winter;
     if (month < 7) return MediaSeason.Spring;
     if (month < 10) return MediaSeason.Summer;
-
     return MediaSeason.Fall;
 };
 
@@ -25,11 +22,9 @@ export const getCurrentSeasonYear = (): string => {
 };
 
 export const getNextSeason = (): MediaSeason => {
-    const month = new Date().getMonth(); // 0 = Jan
-
+    const month = new Date().getMonth() + 1;
     if (month < 4) return MediaSeason.Spring;
     if (month < 7) return MediaSeason.Summer;
     if (month < 10) return MediaSeason.Fall;
-
     return MediaSeason.Winter;
 };
